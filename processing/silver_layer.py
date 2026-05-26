d3a577e7d2028bec7ace6d2e9bd1558c9f465e6f
Gabriel Pereira <gp9075254@gmail.com>
2026-05-26 17:06:11 -0300

    [FEAT] Implementar camada Silver com tipagem de dados

---
+++ processing/silver_layer.py
@@ -0,0 +1,27 @@
+from pyspark.sql.functions import col, to_date
+from processing.utils.spark_session import create_spark_session
+
+
+spark = create_spark_session("SilverLayer")
+
+df = spark.read.parquet("data/bronze/selic")
+
+df_silver = (
+    df
+    .withColumn(
+        "data",
+        to_date(col("data"), "dd/MM/yyyy")
+    )
+    .withColumn(
+        "valor",
+        col("valor").cast("double")
+    )
+)
+
+(
+    df_silver.write
+    .mode("overwrite")
+    .parquet("data/silver/selic")
+)
+
+print("Camada Silver Criada")
